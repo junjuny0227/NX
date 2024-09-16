3a9456aa6477b6c40a6e726dfc26f6b7253065a1
junjuny0227 <junjuny0227@gmail.com>
2024-09-16 16:18:51 +0900

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/Home";
+import Profile from "./routes/profile";
+import Login from "./routes/Login";
+import CreateAccount from "./routes/create-account";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
