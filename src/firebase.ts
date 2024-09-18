063f103cc3add5c7bc0d8895de1f4494a440a112
junjuny0227 <junjuny0227@gmail.com>
2024-09-18 15:13:54 +0900

    Install firebase

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjlckE3gWRBh-NcI-sRknOhIvJAMcGbn8",
+  authDomain: "nx-reloaded-9bb66.firebaseapp.com",
+  projectId: "nx-reloaded-9bb66",
+  storageBucket: "nx-reloaded-9bb66.appspot.com",
+  messagingSenderId: "954068665559",
+  appId: "1:954068665559:web:60134e178f6fd9d3019c92",
+};
+
+const app = initializeApp(firebaseConfig);
