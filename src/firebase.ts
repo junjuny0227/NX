49006a8b07be053cc2f7588b8e10929423148f39
junjuny0227 <junjuny0227@gmail.com>
2024-09-25 22:29:26 +0900

    Hide api key

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjlckE3gWRBh-NcI-sRknOhIvJAMcGbn8",
-  authDomain: "nx-reloaded-9bb66.firebaseapp.com",
-  projectId: "nx-reloaded-9bb66",
-  storageBucket: "nx-reloaded-9bb66.appspot.com",
-  messagingSenderId: "954068665559",
-  appId: "1:954068665559:web:60134e178f6fd9d3019c92",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
