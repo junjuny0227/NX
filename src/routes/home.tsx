366085014c82c6ce0073341c967e494df297bff8
junjuny0227 <junjuny0227@gmail.com>
2024-10-01 00:35:37 +0900

    Fetching Timeline

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 import PostTweetForm from "../components/post-tweet-form";
+import Timeline from "../components/timeline";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 50px;
+  flex-direction: column;
+`;
 
 export default function Home() {
   return (
     <Wrapper>
       <PostTweetForm />
+      <Timeline />
     </Wrapper>
   );
 }
